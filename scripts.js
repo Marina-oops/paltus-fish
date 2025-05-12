ee2f17b1fb4dd88dca92256f9a57f85e5040f0ae
Marina-oops <79970496+Marina-oops@users.noreply.github.com>
2025-05-12 21:07:12 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -55,6 +55,35 @@ document.addEventListener('DOMContentLoaded', () => {
   }); 
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('feedback-form');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputs = form.querySelectorAll('input, textarea');
+    let isValid = true;
+
+    // Простая валидация
+    inputs.forEach(input => {
+      if (!input.value.trim()) {
+        input.style.borderColor = 'red';
+        isValid = false;
+      } else {
+        input.style.borderColor = 'black';
+      }
+    });
+
+    if (!isValid) return;
+
+    // Имитация отправки данных
+    setTimeout(() => {
+      showThankYouFeedbackModal();
+      form.reset();
+    }, 500);
+  });
+});
+
 // Создание и отображение модального окна
 function showThankYouModal() {
   const modal = document.createElement('div');
@@ -67,6 +96,25 @@ function showThankYouModal() {
       <p style="font-weight: 700;">Если заявка отправлена в&nbsp;будни с&nbsp;9:00 до&nbsp;18:00 —&nbsp;мы&nbsp;свяжемся с&nbsp;вами в&nbsp;течение&nbsp;30&nbsp;минут</p>
       <p style="color: white;">Д</p>
       <p>В другое время ответим утром следующего рабочего дня</p>
+    </div>
+  `;
+  document.body.appendChild(modal);
+
+  modal.querySelector('.close-btn').addEventListener('click', () => {
+    modal.remove();
+  });
+}
+
+function showThankYouFeedbackModal() {
+  const modal = document.createElement('div');
+  modal.className = 'modal-overlay';
+  modal.innerHTML = `
+    <div class="modal">
+      <div class="close-btn"><img src="images/close.png" alt="Закрыть "></div>
+      <h2>СПАСИБО ЗА ВАШ ОТЗЫВ!</h2>
+      <p style="font-weight: 700;">Мы признательны вам за&nbsp;вашу&nbsp;оценку! Она&nbsp;будет полезна другим пользователям</p>
+      <p style="color: white;">Д</p>
+      <p>После проверки отзыва на&nbsp;соответствие требованиям, он&nbsp;будет опубликован на&nbsp;сайте! Мы сообщим Вам об&nbsp;этом по&nbsp;электронной почте</p>
     </div>
   `;
   document.body.appendChild(modal);
@@ -129,4 +177,32 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+ // звездочки
 
+document.addEventListener('DOMContentLoaded', () => {
+  const stars = document.querySelectorAll('.star-img');
+  const ratingInput = document.getElementById('ratingValue');
+  let currentRating = 0;
+
+  stars.forEach((star, index) => {
+    // Наведение
+    star.addEventListener('mouseover', () => {
+      stars.forEach((s, i) => {
+        s.src = i <= index ? 'images/star-full.png' : 'images/star-empty.png';
+      });
+    });
+
+    // Убираем наведение
+    star.addEventListener('mouseout', () => {
+      stars.forEach((s, i) => {
+        s.src = i < currentRating ? 'images/star-full.png' : 'images/star-empty.png';
+      });
+    });
+
+    // Клик
+    star.addEventListener('click', () => {
+      currentRating = index + 1;
+      ratingInput.value = currentRating;
+    });
+  });
+});
